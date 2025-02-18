f5b2aa9843f2c592bccb53e97550c78f10f45484
artem.artemkin <artem.artemkin@topvisor.com>
2025-02-18 23:06:51 +0300

    init

---
+++ src/IDeterminedIntegerKeyList.java
@@ -0,0 +1,6 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Set;
+
+public interface IDeterminedIntegerKeyList {
+}
---
+++ src/IUniversalHashFunction.java
@@ -0,0 +1,3 @@
+public interface IUniversalHashFunction {
+    Integer hash(Integer key);
+}
